1075343b194f6eba86561e39d962db3b34a267c4
Kevin McGinty <kevin.mcginty@rovicorp.com>
2013-05-04 19:07:39 -0400

    change migration

--- db/migrate/20130205044538_create_items.rb
+++ db/migrate/20130205044538_create_items.rb
@@ -2,7 +2,7 @@ class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
       t.string :name
-      t.string :price
+      t.int :price
 
       t.timestamps
     end
