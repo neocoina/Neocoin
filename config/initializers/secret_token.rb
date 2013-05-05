f9ade83bd6767696f289ccfb9a9068f74a4410d3
Neocoinage <neocoinage@yahoo.com>
2013-05-05 16:05:23 -0400

    commit

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Neocoin::Application.config.secret_token = 'd83cd72e3ba9f6f4fd1ee014e4e82bfad3800a2c9de67edc324a47e978fd7a06c3f82eee9780c2c41a94f212c2511fc4d2c70f79b8a8173fd6feefa9d7952f33'
